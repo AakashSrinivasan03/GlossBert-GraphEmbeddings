1f3255cb4bf28a0f3397c462f4a01e6fd1d1605c
Aakash <apple@apples-MacBook-Pro.local>
2020-04-30 21:51:04 -0700

    added train files

---
+++ evaluation_script.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset",
+                        default=None,
+                        type=str,
+                        required=True,
+                        choices=['senseval2', 'senseval3', 'semeval2007', 'semeval2013', 'semeval2015', 'ALL'],
+                        help="Dataset name")
+    parser.add_argument("--input_file",
+                        default=None,
+                        type=str,
+                        required=True,
+                        help="Input file of results")
+    
+    args = parser.parse_args()
+
+
+    dataset = args.dataset
+    input_file_name = args.input_file
+    
+    train_file_name = './Evaluation_Datasets/'+dataset+'/'+dataset+'.csv'
+    train_data = pd.read_csv(train_file_name,sep="\t",na_filter=False).values
+    words_train = []
+    for i in range(len(train_data)):
+        words_train.append(train_data[i][4]) # get lemmas
+
+    test_file_name = './Evaluation_Datasets/'+dataset+'/'+dataset+'_test_sent_cls.csv'
+    test_data = pd.read_csv(test_file_name,sep="\t",na_filter=False).values
+
+    seg = [0]
+    for i in range(1,len(test_data)):
+        if test_data[i][0] != test_data[i-1][0]:
+            seg.append(i)
+            
+
+    results=[]
+    num=0
+    with open(input_file_name, "r", encoding="utf-8") as f:
+        s=f.readline().strip()
+        while s:
+            q=float(s.split()[-1])
+            results.append((q,test_data[num][-1]))
+            num+=1
+            s = f.readline().strip()
+
+
+    predicted_sense_keys_dict = {}
+    
+    for i in range(len(seg)):
+        if i!=len(seg)-1:
+            result=results[seg[i]:seg[i+1]]
+        else:
+            result=results[seg[i]:-1]
+        result.sort(key=lambda x:x[0],reverse=True)
+        predicted_sense_keys_dict[test_data[seg[i]][0]] = [result[0][1]]
+
+    gold_dataset_path = './Evaluation_Datasets/'+dataset+'/'+dataset+'.gold.key.txt'
+    gold_sense_keys_dict = {}
+
+    with open(gold_dataset_path, "r", encoding="utf-8") as f:
+        s=f.readline().strip()
+        while s:
+            l = s.split()
+            key=l[0]
+            gold_sense_keys_dict[key] = l[1:]
+            s = f.readline().strip()
+
+
+    ok = 0
+    not_ok = 0
+    for key in predicted_sense_keys_dict:
+        
+        if(key not in gold_sense_keys_dict):
+            continue
+
+        local_ok = len(set(predicted_sense_keys_dict[key]).intersection(gold_sense_keys_dict[key]))/len(set(predicted_sense_keys_dict[key]))
+        local_not_ok = len(set(predicted_sense_keys_dict[key]).difference(gold_sense_keys_dict[key]))/len(set(predicted_sense_keys_dict[key]))
+        ok += local_ok
+        not_ok += local_not_ok
+    P = 100*(ok/(ok+not_ok))
+    R = 100*(ok/len(gold_sense_keys_dict))
+    
+    F = (2*P*R)/(P+R)
+    print("Precision",P)
+    print("Recall",R)
+    print("F1-score",F)
+
+
+
+
+
+
+            
+
+
+if __name__ == "__main__":
+    main()
---
+++ train_classifier.py
@@ -0,0 +1,152 @@
+from __future__ import absolute_import, division, print_function
+
+import argparse
+from collections import OrderedDict
+import csv
+import logging
+import os
+import random
+import sys
+import pandas as pd
+
+import numpy as np
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
+                              TensorDataset)
+from torch.utils.data.distributed import DistributedSampler
+from tqdm import tqdm, trange
+
+from torch.nn import CrossEntropyLoss, MSELoss
+
+
+class FCNet(nn.Module):
+
+    def __init__(self, input_dim,num_labels=2):
+        super(FCNet, self).__init__()
+        self.input_dim = input_dim
+        self.linear_final = nn.Linear(self.input_dim, num_labels)
+
+   
+
+    def forward(self, input_tensor):
+        
+        scores = self.linear_final(input_tensor)
+
+        return scores
+
+
+def load_dataset(path):
+	train_data = np.load(path, allow_pickle=True)
+	train_data = train_data[()]
+	embeddings = train_data['embeddings']
+	labels = train_data['labels']
+	sense_keys = train_data['synsets']
+	return embeddings, labels, sense_keys
+
+def write_results(path,dataset,probs):
+
+	pred = np.argmax(probs,axis=1)
+	
+	with open(os.path.join(path,dataset+'_results.txt'),'w') as f:
+		for i,j,k in zip(pred,probs[:,0],probs[:,1]):
+			f.write(str(i)+' '+str(j)+' '+str(k)+'\n')
+
+
+def main():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("--embeddings_data_dir",default=None,type=str,help="The input embedding file")
+	parser.add_argument("--dataset",default=None,type=str,help="Dataset name")
+	parser.add_argument("--do_train",action='store_true',help="Whether to run training.")
+	parser.add_argument("--do_eval",action='store_true',help="Whether to run evaluation")
+	parser.add_argument("--out_results_dir",default=None,type=str,help="Output result path")
+	parser.add_argument("--load_model_path",default=None,type=str,help="Eval - model to be loaded")
+	parser.add_argument("--batch_size",default=32,type=int,help="Total batch size for training.")
+	parser.add_argument("--num_epochs",default=5,type=int,help="Number of epochs.")
+
+	args = parser.parse_args()
+
+	os.makedirs(args.out_results_dir, exist_ok=True)
+
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	n_gpu = torch.cuda.device_count()
+
+	embeddings, labels,sense_keys = load_dataset(args.embeddings_data_dir)
+	all_labels = labels
+	embeddings = torch.tensor(embeddings).float()
+	labels = torch.tensor(labels).long()
+
+	data= TensorDataset(embeddings, labels)
+	shuffle_bool = not args.do_eval
+	dataloader = DataLoader(data, batch_size=args.batch_size, shuffle=shuffle_bool)
+	num_labels = 2
+
+	best_tr_loss = float('inf')
+	if args.do_train:
+
+		output_model_file = os.path.join(args.out_results_dir,"model_save")
+		model = FCNet(embeddings.shape[1])
+		model.train()
+		optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+		gamma = 0.99
+		scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma, last_epoch=-1)
+		
+		epoch = 0
+		loss_fct = CrossEntropyLoss()
+		for epoch_no in trange(int(args.num_epochs), desc="Epoch"):
+
+			epoch += 1
+			tr_loss = 0
+			
+			for step, batch in enumerate(tqdm(dataloader, desc="Iteration")):
+
+				batch = tuple(t.to(device) for t in batch)
+				inputs, labels = batch
+
+				logits = model(inputs)
+
+				loss = loss_fct(logits.view(-1, num_labels), labels.view(-1))
+				tr_loss += loss
+				loss.backward()
+				optimizer.step()
+				optimizer.zero_grad()
+			print("Epoch",epoch_no,"Loss",tr_loss)
+			best_tr_loss = min(best_tr_loss,tr_loss)
+			if(best_tr_loss==tr_loss):
+				torch.save(model.state_dict(), output_model_file)
+		probs = nn.Softmax(dim=-1)(model(embeddings)).cpu().detach().numpy()
+		pred = (probs[:,1]>=0.5).astype(int)
+		truth = all_labels.astype(int)
+		
+		print("accuracy",np.sum(pred==truth)*1.0/pred.shape[0])
+		write_results(args.out_results_dir,args.dataset,probs)
+	if(args.do_eval):
+		model = FCNet(embeddings.shape[1])
+		model.load_state_dict(torch.load(args.load_model_path))
+		
+		model.eval()
+		logits = model(embeddings)
+
+		probs = nn.Softmax(dim=-1)(logits).cpu().detach().numpy()
+		pred = (probs[:,1]>=0.5).astype(int)
+		truth = all_labels.astype(int)
+		
+		print("accuracy",np.sum(pred==truth)*1.0/pred.shape[0])
+		write_results(args.out_results_dir,args.dataset,probs)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
